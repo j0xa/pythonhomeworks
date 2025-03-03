c35243eef1841f728d69d9af6734c34803d7e03b
j0xa <abdirashitovjahongir123@gmail.com>
2025-03-03 20:08:03 +0500

    homework

--- lesson-2/homework/boolean.py
+++ lesson-2/homework/boolean.py
@@ -1,3 +1,36 @@
+#q1
+username=str(input("Enter the username: "))
+password=str(input("Enter the password: "))
+if not bool(username.strip()) or not bool(password.strip()):
+    print("fill up the blank space")
+
+#q2
+first=int(input("enter the first nnumber: "))
+second=int(input("enter the second number: "))
+if bool(first==second):
+    print("theyre the same number")
+
+#q3
+number=int(input("Enter the number: "))
+if bool(number%2==0) and bool(number>0):
+    print('it is positive and even')
+else:print("it is not positive and even")
+
+#q4
+num1=int(input("Enter the 1st number: "))
+num2=int(input("Enter the 2nd number: "))
+num3=int(input("Enter the 3rd number: "))
+if bool(num1!=num2) and bool(num2!=num3) and bool(num1!=num3):
+    print("theyre different")
+else:
+    print("theyre not different")
+
+#q5
+txt1=str(input("Enter the text 1: "))
+txt2=str(input("Enter the text 2: "))
+if bool(len(txt1)==len(txt2)):
+    print("they have the same length")
+else: print("they have different length")
 
 #q6
 a=int(input("Enter the number: "))
@@ -5,3 +38,18 @@ if bool(a%3==0) and bool(a%5==0):
     print(" it is divisible by both 3 and 5")
 else:
     print(" it is not divisible by both 3 and 5")
+
+#q7
+b=float(input("enter the number: "))
+c=float(inout("Enter the number: "))
+d=c+b
+if bool(d>50.8):
+    print("Their summ is greate than 50.8")
+else:
+    print("Their summ is not greate than 50.8")
+
+#q8
+e=float(input("enter the number"))
+if bool(e>10)and bool(e<20):
+    print("the given number is in between 10 and 20")
+else:print("it is not between 10 and 20")
--- lesson-2/homework/string.py
+++ lesson-2/homework/string.py
@@ -1,3 +1,117 @@
+#q1
+name=str(input("Enter your name: "))
+year=int(input("Enter your year of birth: "))
+age=2025-year
+print("You are ",name, " and you are", age, "years old")
+
+#q2
+
+#q3
+text=str(input("Enter your text: "))
+print(len(text))
+print(text.lower())
+print(text.upper())
+
+#Q4
+word=str(input("Enter your word: "))
+if word==word[::-1]:
+    print("your word is palindrome")
+else:
+    print("It is not a palindrome")
+
+#q5
+vowel=0
+consonant=0
+my_str=str(input("Enter your word:"))
+vowels="euioa"
+for i in my_str.strip():
+    if i in vowels:
+        vowel=vowel+1
+    else:
+        consonant=consonant+1
+print(vowel)
+print(consonant)
+
+#q6
+first=str(input("Enter your main text: "))
+second=str(input("Enter the word to be searched: "))
+if second in first:
+    print("string found")
+else:
+    print("not found")
+
+#q7
+sentence=str(input("Enter the sentence: "))
+word=str(input("enter the word:"))
+replacement=str(input("enter the word:"))
+print(sentence.replace(word, replacement))
+
+#q8
+txt=str(input("enter the text: "))
+start=txt[0]
+end=txt[-1]
+print(start)
+print(end)
+
+#q9
+ent=str(input("enter the text: "))
+print(ent[::-1])
+
+#q10
+text=str(input("Enter the text: "))
+count=len(text.split())
+print(count)
+
+#q11
+digits="1234567890"
+q=input("enter the stuff: ")
+t=0
+for i in q:
+    if i in digits:
+        t=t+1
+    else:
+        t=t
+if t!=0:
+    print("there is a digit")
+else:
+    print("there is no digit")
+
+#q12
+y=str(input("enter the sentence: "))
+u=[" ",",","."]
+for i in y:
+    if i in u:
+        y=y.replace(i,"")
+print(y)
+
+#q13
+o=str(input("Enter the text: "))
+p=o.replace(" ", "")
+print(p)
+
+#q14
+idk=str(input("enter the first: "))
+idkk=str(input("enter the second: "))
+if idk==idkk:
+    print("theyre same")
+else:
+    print("theyre different")
+
+#q15
+full=str(input("enter the full form: "))
+acr="".join(x[0].upper() for x in full.split())
+print(acr)
+
+#q16
+g=str(input("Enter the string: "))
+char=str(input("Enter the char to remove: "))
+g=g.replace(char, "")
+print(g)
+
+#q17
+h=str(input("enter the text: "))
+chara=str(input("enter the character to be removed: " ))
+print(h.replace(chara, ""))
 
 #q18 
 j=str(input("Entrer the sentence: "))
