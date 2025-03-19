887539993ad16ab12e39ac73fe7fa230142c960c
j0xa <abdirashitovjahongir123@gmail.com>
2025-03-19 18:53:40 +0500

    smth

--- lesson-3/homework/list tasks.py
+++ lesson-3/homework/list tasks.py
@@ -1,1 +1,17 @@
-print("t")
+n=int(input("enter the number of items to be entered: "))
+a = [input(f"Enter element {i+1}: ") for i in range(n)]
+if n==0:
+    print("the list is empty")
+else:
+    print(a[0])
+    print(a[-1])
+newlist=a[0:3]
+print(newlist)
+print(sorted(a, reverse=True))
+a.sort()
+print(a)
+b=list(set(a))
+print(b)
+c=input("Enter the item to be added: ")
+a.append(c)
+print(a)
